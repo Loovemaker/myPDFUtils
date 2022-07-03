39d850ced5f484e52d9cb2e5d446f3bebc02504f
Loovemaker <bows-02.roper@icloud.com>
2022-07-03 17:43:43 +0800

    `setup.py` for Windows(using `py2exe`)
    >   `py2exe` is on the opposition of human-friendliness
    >
    >   I'll get another solution for building `.exe`

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from setuptools import setup
-from setup_options.setup_py2app import setup_kwargs
+from sys import argv
+
+assert len(argv) >= 2
+platform = argv[1]
+
+if platform == 'py2exe':
+    import py2exe
+    from setup_options.setup_py2exe import setup_kwargs
+elif platform == 'py2app':
+    import py2app
+    from setup_options.setup_py2app import setup_kwargs
+else:
+    raise ValueError()
 
 setup(**setup_kwargs)
